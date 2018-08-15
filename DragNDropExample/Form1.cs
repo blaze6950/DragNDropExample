94a06c5472c75e80b0d714077229466d623dca61
User <User@WIN-GQOKRARBRRP>
2018-08-15 13:28:07 +0300

    Add project files.

---
+++ DragNDropExample/Form1.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DragNDropExample
+{
+    public partial class Form1 : Form
+    {
+        bool dragstate = false;
+        public Form1()
+        {
+            InitializeComponent();
+            
+            // разрешаем списку стать адресатом буксировки
+            listBox1.AllowDrop = true;
+        }
+        private void textBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            // при опускании клавиши мыши выполняем буксировку
+            // содержимого источника
+            textBox1.DoDragDrop(textBox1.Text, DragDropEffects.Copy);
+        }
+        private void listBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            // при попадании на адресат формируем соответствующую иконку 
+            // для курсора
+            if (e.Data.GetDataPresent(DataFormats.StringFormat)) 
+				e.Effect = DragDropEffects.Copy;
+			else
+				e.Effect = DragDropEffects.None;
+        }
+        private void listBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            // при отпускании кнопки производим копирование данных в элемент списка
+            listBox1.Items.Add(e.Data.GetData(DataFormats.StringFormat).ToString());
+            dragstate = false;
+        }
+
+      
+                
+        
+        
+
+    }
+}
